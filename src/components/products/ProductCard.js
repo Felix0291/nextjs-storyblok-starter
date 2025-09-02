a01acf43abd61c49bb24a0febacb70827a97e6c9
ErikBergloef <47384098+ErikBergloef@users.noreply.github.com>
2025-09-02 11:53:08 +0200

    product card

--- src/components/products/ProductCard.js
+++ src/components/products/ProductCard.js
@@ -1,15 +1,29 @@
-
 import { storyblokEditable } from "@storyblok/react";
+import Link from "next/link";
+import Image from "next/image";
 
-export default function ProductCard({ blok }) {
-
-
-
-    return (
-        <div {...storyblokEditable(blok)} className={heroClasses} style={{
-            backgroundImage: `url(${blok?.background_image?.filename})`
-        }}>
-            
-        </div>
-    )
+export default function ProductCard({ title, price, image, slug }) {
+  return (
+    <Link
+      href={`/products/${slug}`}
+      className="border rounded-lg p-4 hover:shadow-lg transition-shadow duration-300"
+    >
+      <div className="w-full h-48 relative mb-4">
+        {image ? (
+          <Image
+            src={image}
+            alt="Product Image"
+            width={600}
+            height={400}
+            style={{ objectFit: "cover", width: "100%", height: "100%" }}
+          />
+        ) : (
+          <div className="w-full h-full bg-gray-200 flex items-center justify-center">
+            {" "}
+            No Image{" "}
+          </div>
+        )}
+      </div>
+    </Link>
+  );
 }
